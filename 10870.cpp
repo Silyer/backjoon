f758eccc288092b11876315f947a3600da4b64a0
Silyer <81630050+Silyer@users.noreply.github.com>
2021-04-02 23:23:09 +0900

    Create 10870.cpp

---
+++ 10870.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int d[21] = {0};
+	d[0] = 0, d[1] = 1;
+	for(int i = 2; i <= n; i++) d[i] = d[i - 1] + d[i - 2];
+	cout << d[n];
+	return 0;
+}
