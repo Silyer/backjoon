dccc59bf5b34c9f99ae9f5da76ab6c56484a29a0
Silyer <81630050+Silyer@users.noreply.github.com>
2021-04-12 23:34:18 +0900

    Create 5063.cpp

---
+++ 5063.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	while(n--){
+		int r, e, c;
+		cin >> r >> e >> c;
+		if(r < e - c) cout << "advertise\n";
+		else if (r == e - c) cout << "does not matter\n";
+		else cout << "do not advertise\n";
+	}
+	return 0;
+}
