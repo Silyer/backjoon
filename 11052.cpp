6788f5753b64a63f43d6bad45f1a27a2d93b0374
Silyer <81630050+Silyer@users.noreply.github.com>
2021-04-15 22:19:07 +0900

    Create 11052.cpp

---
+++ 11052.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, m = 0;
+	cin >> n;
+	
+	int *p = new int[n + 1];
+	int *d = new int[n + 1];
+	for(int i = 1; i <= n; i++) cin >> p[i];
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= i; j++) {
+			d[i] = max(d[i], d[i - j] + p[j]);
+			if(m < d[i]) m = d[i];
+		}
+	}
+	cout << m;
+	// d[n] = max(d[n- i] + p[i])
+	// 1 <= i <= n
+	return 0;
+}
