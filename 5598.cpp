e0e8abeb21b7ba04102e4bb8f349f9789d2d4266
Silyer <81630050+Silyer@users.noreply.github.com>
2021-05-24 22:03:09 +0900

    Update 5598.cpp
    corrected the error

--- 5598.cpp
+++ 5598.cpp
@@ -5,12 +5,9 @@ using namespace std;
 int main() {
 	string s;
 	cin >> s;
-	for(int i = 0; i < s.size(); i++) {
-			if(s[i] < 44) s[i] += 20;
-	}
 
 	for(int i = 0; i < s.size(); i++) {
-		
+		if(s[i] <= 'C') s[i] += 26;
 		s[i] -= 3;
 		cout << s[i];
 	}
