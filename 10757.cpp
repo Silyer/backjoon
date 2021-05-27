5e7879452a598e02ad1c38c4728c4d3e13cf5581
Silyer <81630050+Silyer@users.noreply.github.com>
2021-05-27 22:20:25 +0900

    Update 10757.cpp
    solved counter cases.

--- 10757.cpp
+++ 10757.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//9999 + 99
-//99 + 9999 반례 이해
 int main() {
 	string a, b;
 	int sum[10003] = {0}, j = 1;
 	cin >> a >> b;
-	if(a.size() == b.size()) {
+	if(a.size() == b.size()) {			//두 수의 자릿수 동일
 		for(int i = 0; i <= a.size(); i++) {
 			int an = a[i] - '0', bn = b[i] - '0';
-			if(an + bn >= 10){
-				sum[j - 1] += 1;
-				sum[j] += (an + bn) % 10;
-			} 
-			else sum[j] += an + bn;
+			sum[j] += an + bn;
 			j++;
+		}
+		for(int i = a.size(); i > 0; i--) { 	//자릿수 10올림
+			if(sum[i] >= 10) {
+				sum[i - 1] += 1;
+				sum[i] %= 10;
+			}
 		}
 		for(int i = 0; i <= a.size(); i++)
 			if(sum[0] != 0 || i != 0) cout << sum[i];
 	}
-	else if(a.size() > b.size()) {
+	else if(a.size() > b.size()) {			//자릿수가 서로 다름
 		for(int i = 0; i < a.size() - b.size(); i++) {
 			sum[i] = a[i] - '0';
 		}
 		for(int i = a.size() - b.size(); i < a.size(); i++) {
 			int an = a[i] - '0', bn = b[j - 1] - '0';
-			if(an + bn >= 10){
-				sum[i - 1] += 1;
-				sum[i] += (an + bn) % 10;
-			} 
-			else {
-				sum[i] += an + bn;
-			}
+			sum[i] += an + bn;
 			j++;
+		}
+		for(int i = a.size() - 1; i > 0; i--) {
+			if(sum[i] >= 10) {
+				sum[i - 1] += 1;
+				sum[i] %= 10;
+			}
 		}
 		for(int i = 0; i < a.size(); i++)
 			if(sum[0] != 0 || i != 0) cout << sum[i];
@@ -44,14 +44,14 @@ int main() {
 		}
 		for(int i = b.size() - a.size(); i < b.size(); i++) {
 			int an = a[j - 1] - '0', bn = b[i] - '0';
-			if(an + bn >= 10){
-				sum[i - 1] += 1;
-				sum[i] += (an + bn) % 10;
-			} 
-			else {
 				sum[i] += an + bn;
-			}
 			j++;
+		}
+		for(int i = b.size() - 1; i > 0; i--) {
+			if(sum[i] >= 10) {
+				sum[i - 1] += 1;
+				sum[i] %= 10;
+			}
 		}
 		for(int i = 0; i < b.size(); i++)
 			if(sum[0] != 0 || i != 0) cout << sum[i];
