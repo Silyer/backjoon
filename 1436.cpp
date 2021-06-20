482fa99b213aa67e3cf743e28e5b6f4c3fced6f3
Silyer <81630050+Silyer@users.noreply.github.com>
2021-06-20 23:16:00 +0900

    Update 1436.cpp

--- 1436.cpp
+++ 1436.cpp
@@ -1,30 +1,26 @@
-//test
 #include <iostream>
-#include <algorithm>
 #include <string>
 using namespace std;
 
 int main() {
-	int n, d = 1666, m[10001] = {0};
+	int n, d = 666, m[10001] = {0}, i = 0;
 	cin >> n;
-	for(int i = 0; i < 1; i++) {
+	while(i != 10000) {
 		int si = 0;
 		string s = to_string(d);
-		cout << "d = " << d << "\n";
 		for(int j = 0; j < s.size() - 2; j++) {
 			for(int k = j; k < j + 3; k++) {
-				cout << "s[" << k << " = " << s[k] << "\n"; 
 				if(s[k] == '6') si++;
 			}
-			cout << "si = " << si << "\n";
 			if(si >= 3) {
-				m[i] = d;
+				m[i++] = d;
 				break;
 			}
 			else si = 0;
 		}
 		d++;
 	}
-	cout << m[0] << " " << m[1];
+	cout << m[n - 1];
 	return 0;
 }
+//이것보다도 더 단순하게 하는 방법 : 그냥 1000으로 나눈 나머지가 666이 되는지 bool..
