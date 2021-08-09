e8f6680479550f5f8cb7b70fe0e370da3733227a
Silyer <81630050+Silyer@users.noreply.github.com>
2021-08-09 22:46:52 +0900

    Create 15654.cpp

---
+++ 15654.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool vi[10];
+int arr[10];
+int num[10];
+
+void dfs(int n, int m, int d) {
+	if(d == m) {
+		for(int i = 0; i < m; i++) cout << num[arr[i]] << " ";
+		cout << '\n';
+		return;
+	}
+	
+	for(int i = 0; i < n; i++) {
+		if(vi[i]) continue;
+		vi[i] = true;
+		arr[d] = i;
+		dfs(n, m, d + 1);
+		vi[i] = false;
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	
+	for(int i = 0; i < n; i++) cin >> num[i];
+	sort(num, num + n);
+	dfs(n, m, 0);
+	return 0;
+}
