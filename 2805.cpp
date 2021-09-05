79a7a3b31e3484a996852b315eb74a55c0c188a9
Silyer <81630050+Silyer@users.noreply.github.com>
2021-09-05 22:41:05 +0900

    Update 2805.cpp

--- 2805.cpp
+++ 2805.cpp
@@ -1,9 +1,9 @@
-#include <iostream>             //아직 미완
+#include <iostream>            
 #include <vector>
 using namespace std;
 
 int main() {
-	long long n, m, st = 1, end = 0, min = 9999999999;
+	long long n, m, st = 1, end = 0, re = 0;
 	cin >> n >> m;
 	
 	vector<long long> t(n, 0);
@@ -12,18 +12,18 @@ int main() {
 		if(end < t[i]) end = t[i];
 	}
 	
-	while(st <= end) {
+	while(st <= end) {					//이분 탐색
 		long long mid = (st + end) / 2, cut = 0;
 		for(int i = 0; i < n; i++){
-			cut += t[i] - end;
-			if(cut < 0) cut = 0;
+			if(t[i] > mid) cut += t[i] - mid;
 		} 
+		
 		if(cut >= m) {
-			if(min > mid) min = mid;
+			if(re < mid) re = mid;
 			st = mid + 1;
 		}
 		else end = mid - 1;
 	}
-	cout << min;
+	cout << re;
 	return 0;
 }
