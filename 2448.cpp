b96e3df7d5d9f96fb811e85e2d1bc8d3e1a7513a
Silyer <81630050+Silyer@users.noreply.github.com>
2021-09-29 23:48:57 +0900

    Update 2448.cpp

--- 2448.cpp
+++ 2448.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 using namespace std;
-
+char a[10000][10000];
 void star(int n, int i, int j) {
-	if(i * 2 > n && j >= 2 * i - n && i >= j) cout << ' ';
-	else if (n / 3 == 1) cout << '*';
-	else star(n / 2, i, j);
+	if(n == 3) {						//n = 3일때 나오는 삼각형 출력. (규칙으로 안됨)
+		a[i][j] = '*';
+		a[i + 1][j - 1] = '*';
+		a[i + 1][j + 1] = '*';
+		a[i + 2][j - 2] = '*';
+		a[i + 2][j - 1] = '*';
+		a[i + 2][j] = '*';
+		a[i + 2][j + 1] = '*';
+		a[i + 2][j + 2] = '*';
+	}
+	else {							//n이 그 이상이면 
+		star(n / 2, i, j);				//꼭대기에서
+		star(n / 2, i + n / 2, j - n / 2);		//왼쪽 꼭짓점에서
+		star(n / 2, i + n / 2, j + n / 2);		//오른쪽 꼭짓점으로 재귀
+	}
 }
+
 int main() {
 	int n;
 	cin >> n;
-	for(int i = 1; i <= n; i++) {
-		for(int b = n - i; b > 0; b--) cout << ' ';
-		for(int j = 1; j < i * 2; j++) {
-			star(n, i, j);
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n * 2 - 1; j++) {
+			a[i][j] = ' ';
+		}
+	}
+	star(n, 0, n - 1);
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n * 2 - 1; j++) {
+			cout << a[i][j];
 		}
 		cout << '\n';
 	}
 	return 0;
 }
-/*
-* *        (2, 2)
-*****									   4	  5	   6
-*     * 	(4, 2 ~ 6) 6 : i = 4 ~ 6 j = 2 ~ 6, 4 ~ 6, 6
-* *   * *	(5, 4 ~ 6)
-***** *****	(6, 6)		(x + n) / 2 = i // x = 2 * i - n
-*           *			(7, 2 ~ 12)	12 : 7 ~ 12
-* *         * *			(8, 4 ~ 12)
-*****       *****		(9, 6 ~ 12)
-*     *     *     * 	(10, 8 ~ 12)
-* *   * *   * *   * *	(11, 10 ~ 12)
-***** ***** ***** *****/
