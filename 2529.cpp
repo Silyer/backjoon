0c1ce2f7e4a984d58318b03fe17e09ed9bf1bf88
Silyer <81630050+Silyer@users.noreply.github.com>
2021-10-09 22:30:20 +0900

    Update 2529.cpp

--- 2529.cpp
+++ 2529.cpp
@@ -7,29 +7,34 @@ int n;
 char k[10];
 vector<string> re;
 bool ok[10];
-bool comp(char a, char b, char c) {
-	if(c == '<') if (a > b) return false;
-	if(c == '>') if (a < b) return false;
+bool comp(string s) {							
+	for(int i = 0; i < n; i++) {
+		if(k[i] == '<') if (s[i] > s[i + 1]) return false;
+		if(k[i] == '>') if (s[i] < s[i + 1]) return false;
+	}
 	return true;
 }
 
 void solve(int idx, string num) {
 	if(idx == n + 1) {
-		re.push_back(num);
+		if(comp(num)) {
+			re.push_back(num);
+		}
 		return;
 	}
 	for(int i = 0; i <= 9; i++) {
 		if(ok[i]) continue;
-		if(idx == 0 || comp(num[idx - 1], i + '0', k[idx - 1])) {
-			ok[i] = true;
-			solve(idx + 1, num + to_string(i));
-		}
+		ok[i] = true;
+		solve(idx + 1, num + to_string(i));
+		ok[i] = false;
 	}
 }
 int main() {
 	cin >> n;
 	for(int i = 0; i < n; i++) cin >> k[i];
 	solve(0, "");
-  //다른 건 다 끝냈는데 최소, 최댓값 출력 구현 
+	auto p = minmax_element(re.begin(), re.end());
+    cout << *p.second << '\n';
+    cout << *p.first << '\n';
 	return 0;
 }
