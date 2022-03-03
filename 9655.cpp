ed92afab452221615a01e23fb9367adad00f4f2c
Silyer <81630050+Silyer@users.noreply.github.com>
2022-03-03 23:23:38 +0900

    Create 9655.cpp

---
+++ 9655.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n % 2) cout << "SK";
+	else cout << "CY";
+	return 0;
+}
+/*	5
+sk	3	1 win		1	1 win		1	3	win		1	1	1	win
+cy	1			  	3			  	1			  	1	1
+*/	
